807f6d4ec46edb7604e840d63e2a1271201c5168
abboskhonov <berkino2099@gmail.com>
2026-04-15 23:34:56 +0500

    clean up

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -2,10 +2,12 @@ import "dotenv/config"
 import app from "./app"
 
 const port = process.env.PORT || 3001
+const hostname = "0.0.0.0"
 
 Bun.serve({
   fetch: app.fetch,
   port: Number(port),
+  hostname,
 })
 
-console.log(`🚀 API Server running at http://localhost:${port}`)
+console.log(`🚀 API Server running at http://${hostname}:${port}`)
